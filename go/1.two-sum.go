b8cb184df7efcc535e94c19bd09c7b09f9b2641c
lightning <lightning@lightningnoMacBook-Air.local>
2021-09-29 16:17:37 +0900

    WIP:GOわからん

---
+++ go/1.two-sum.go
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ */
+
+package main
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	return []int{0, 1}
+}
+
+// @lc code=end
